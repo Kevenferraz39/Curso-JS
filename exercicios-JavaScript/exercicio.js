8fba47eca70bd450791b4aeaaa8b0b23ddd75d93
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2023-11-28 21:53:33 -0300

    .

---
+++ exercicios-JavaScript/exercicio.js
@@ -0,0 +1,13 @@
+//mudar a pagina
+
+function VoltaPagina(){
+    var voltaPagina = "/Aula-11/ex015.html"//Definir para que pagina esse botao ira levar
+    window.location.href = voltaPagina;//Voltar para pagina anterior
+}
+    //esta funcao abaixo passar para a proxima pagina
+
+function MudarPagina() {
+var novaPagina = "/Aula-11/ex016.html";//Definir para que pagina esse botao ira levar 
+
+window.location.href = novaPagina;//Mudar para a proxima pagina
+}
