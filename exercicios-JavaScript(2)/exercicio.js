eb59d5e18ee0ba54e991fa7114e5fd568862515c
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2023-11-28 23:01:52 -0300

    atualizacao

---
+++ exercicios-JavaScript(2)/exercicio.js
@@ -0,0 +1,16 @@
+//mudar a pagina
+
+function VoltaPagina(){
+    var voltaPagina = "exercicios-JavaScript/exercicio.html"//Definir para que pagina esse botao ira levar
+    window.location.href = voltaPagina;//Voltar para pagina anterior
+}
+    //esta funcao abaixo passar para a proxima pagina
+
+function MudarPagina() {
+var novaPagina = "/Aula-11/ex016.html";//Definir para que pagina esse botao ira levar 
+
+window.location.href = novaPagina;//Mudar para a proxima pagina
+}
+
+//exercicio 
+
--- exercicios-JavaScript/exercicio.js
+++ exercicios-JavaScript/exercicio.js
@@ -7,7 +7,30 @@ function VoltaPagina(){
     //esta funcao abaixo passar para a proxima pagina
 
 function MudarPagina() {
-var novaPagina = "/Aula-11/ex016.html";//Definir para que pagina esse botao ira levar 
+var novaPagina = "exercicios-JavaScript(2)/exercicio.html";//Definir para que pagina esse botao ira levar 
 
 window.location.href = novaPagina;//Mudar para a proxima pagina
+}
+
+//exercicio 
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var date = new Date()
+    var hora = date.getHours()
+    msg.innerHTML = `Agora sao ${hora} horas`
+    if(hora >= 0 && hora < 12){
+        //Bom dia
+        img.src = 'manha.jpg'
+        document.body.style.background = 'rgb(166, 213, 218)'
+    }else if(hora >= 12 && hora <= 18){
+        //Boa tarde
+        img.src = 'tarde.jpg'
+        document.body.style.background = 'rgb(244, 190, 97)'
+    }else{
+        //Boa noite
+        img.src = 'noite.jpg'
+        document.body.style.background = 'rgb(7, 7, 72)'
+       
+    }
 }
